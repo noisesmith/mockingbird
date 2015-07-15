9494de7324f493e1e207e96079f4b00923b3ad21
Justin Glenn Smith <noisesmith@gmail.com>
2015-07-14 20:18:16 -0700

    playing with stuff

--- src/org/noisesmith/mockingbird.clj
+++ src/org/noisesmith/mockingbird.clj
@@ -2,6 +2,43 @@
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "birbs"
   [& args]
-  (println "Hello, World!"))
+  (doto :chirp-chirp
+    println))
+
+(defn validator-one
+  [datum]
+  (let [mdtau (set (take 3 (shuffle datum)))]
+    (assert (and (contains? mdtau :red)
+                 (contains? mdtau :yellow))
+            "no bad flower bed")
+    (contains? mdtau :blue)))
+
+(defn has?
+  [color a b c]
+  (contains? (hash-set a b c) color))
+
+(defn pick-three
+  [a b c]
+  {:pre [(has? :red a b c)
+         (has? :yellow a b c)]}
+  (has? :blue a b c))
+
+(defn all-possible-beds
+  [flowers]
+  (let [next-flowers (mapcat (fn [flower-list]
+                               [(cons :red flower-list)
+                                (cons :blue flower-list)
+                                (cons :yellow flower-list)])
+                             flowers)]
+    (println "generated" next-flowers)
+    (lazy-seq
+     (concat next-flowers
+           (all-possible-beds
+            next-flowers)))))
+
+(defn puzzle-one
+  "the answer"
+  []
+  )
